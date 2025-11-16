4424d473e88a91e0a1fcd602099f562b8684d9af
justinsgithub3 <justinsrecallback@gmail.com>
2025-11-16 15:59:02 -0500

    - added graphs to overview

--- public/assets/js/overview.js
+++ public/assets/js/overview.js
@@ -8,7 +8,8 @@ document.addEventListener("DOMContentLoaded", async () => {
   await reDisplayProfit();
 
   const data = await getAccounts();
-  await createChart(data);
+  await createPlotChart(data);
+  await createLineChart(data);
 })
 
 
@@ -39,8 +40,94 @@ async function getAccounts () {
 
 }
 
+async function createLineChart(data) {
+  const avgIncomes = data.avgIncomes;
+  const avgExpenses = data.avgExpenses;
 
-async function createChart(data) {
+
+
+    const xyAvgIncomes = avgIncomes.map(avgIncome => ({
+    x: new Date(avgIncome.income_date),       // x-axis = date
+    y: parseFloat(avgIncome.avg_daily_amount) // y-axis = amount (as number)
+  }));
+
+  const xyAvgExpenses = avgExpenses.map(avgExpense => ({
+    x: new Date(avgExpense.expense_date),       // x-axis = date
+    y: parseFloat(avgExpense.avg_daily_amount)  // y-axis = amount (as number)
+  }));
+
+  new Chart("myLineChart", {
+  type: "line",
+  data: 
+  {
+    datasets: [
+      {
+      label: "income",
+      pointRadius: 4,
+      pointBackgroundColor: "rgba(0,255,0, 1)",   // point color
+      borderColor: "rgba(0,255,0,1)",             // line color and border of each point
+      backgroundColor: "rgba(0,255,0,1)",         // legend fill
+      data: xyAvgIncomes
+      },
+      {
+      label: "expense",
+      pointRadius: 4,
+      pointBackgroundColor: "rgba(255,0,0,1)",    // point color
+      borderColor: "rgba(255,0,0,1)",             // line color and border of each point
+      backgroundColor: "rgba(255,0,0,1)",         // legend fill
+      data: xyAvgExpenses
+      }
+
+  ]
+  },
+  options: 
+  {
+    plugins: {
+          title: {
+            display: true,
+            text: "daily average",
+            font: {
+              size: 20,
+              weight: "bold"
+            },
+            padding: {
+              top: 10,
+              bottom: 20
+            }
+          }
+        },
+    responsive: false,
+    maintainAspectRatio: false,
+    scales: 
+    {
+      x: 
+        {
+          type: 'time', 
+          time: {
+            unit: 'day',          // adjust to 'month'/'week' if needed
+            tooltipFormat: 'yyyy-MM-dd'
+          },
+          title: {
+            display: true,
+            text: 'date'
+          }
+        },
+      y: 
+        {
+          title: 
+          {
+            display: true,
+            text: 'amount'
+          }
+        }
+    }
+  }
+});
+
+}
+
+
+async function createPlotChart(data) {
   const incomes = data.incomes;
   const expenses = data.expenses;
 
@@ -54,7 +141,7 @@ async function createChart(data) {
     y: parseFloat(expense.amount)        // y-axis = amount (as number)
   }));
 
-  new Chart("myChart", {
+  new Chart("myPlotChart", {
   type: "scatter",
   data: 
   {
@@ -62,13 +149,15 @@ async function createChart(data) {
       {
       label: "income",
       pointRadius: 4,
-      pointBackgroundColor: "rgba(0,255,0, 1)",
+      pointBackgroundColor: "rgba(0,255,0, 1)",   // color of each point
+      borderColor: "rgba(0,255,0,1)",             // line border of each point
       data: xyIncomes
       },
       {
       label: "expense",
       pointRadius: 4,
-      pointBackgroundColor: "rgba(255,0,0,1)",
+      pointBackgroundColor: "rgba(255,0,0,1)",    // color of each point
+      borderColor: "rgba(255,0,0,1)",             // line border of each point
       data: xyExpenses
       }
 
@@ -76,6 +165,22 @@ async function createChart(data) {
   },
   options: 
   {
+        plugins: {
+          title: {
+            display: true,
+            text: "all recorded values",
+            font: {
+              size: 20,
+              weight: "bold"
+            },
+            padding: {
+              top: 10,
+              bottom: 20
+            }
+          }
+        },
+    responsive: false,
+    maintainAspectRatio: false, 
     scales: 
     {
       x: 
