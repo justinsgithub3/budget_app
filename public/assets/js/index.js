fb76ae8599e3d162a12dd3807e51d5ea4026cc15
justinsgithub3 <justinsrecallback@gmail.com>
2025-11-21 10:54:34 -0500

    - added hello,... - centered footer on index

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,9 +1,17 @@
 const header = document.querySelector(".header");
 // create element to add current user's name
 const nameEle = document.createElement("p");
+
 //get name from session storage
-const name = sessionStorage.getItem('name')
-nameEle.textContent = name;
+const userName = sessionStorage.getItem('name');
+
+// if there is no name, assign 'hello, friend'
+if (!userName) {
+    nameEle.textContent = 'hello, friend!';
+} else {
+    nameEle.textContent = `hello, ${userName}!`;
+}
+
 
 // get list of children
 let list = header.children;
